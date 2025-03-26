3b40a19ba505f89faa46536b5c35bd3732b215af
Judith Espinal <j.espinalm12@gmail.com>
2025-03-26 08:15:39 -0400

    Update script config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,35 @@
 module.exports = {
   env: {
     browser: false,
-    es2021: true,
-    node: true,
+    es6: true,
+    jest: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
-}; 
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
