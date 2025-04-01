82768c7e96244173f8b3fd3353f997695dd5c3b1
Judith Espinal <j.espinalm12@gmail.com>
2025-04-01 10:42:56 -0400

    add: implement getResponseFromAPI function with Promise.resolve

--- ES6_promises/0-promise.js
+++ ES6_promises/0-promise.js
@@ -1,5 +1,3 @@
-export default function getResponseFromAPI() { // return a promise
-  return new Promise((resolve) => { // function that takes a value and resolves the promise
-    resolve();
-  });
+export default function getResponseFromAPI() {
+  return Promise.resolve({ status: 200, body: 'Success' });
 }
