82c427f5e2f911d26b3d325519d27bc16e566f31
Judith Espinal <j.espinalm12@gmail.com>
2025-04-02 09:15:42 -0400

    fix: handleProfileSignup to return formatted allSettled results

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,20 @@
 import signUpUser from './4-user-promise'; // import the signUpUser function
 import uploadPhoto from './5-photo-reject'; // import the uploadPhoto function
 
-export default function handleProfileSignup( // return a promise
-  firstName,
-  lastName,
-  fileName,
-) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   // create an array of promises
-  return Promise.allSettled(promises); // return a promise that resolves to an array of results
+
+  return Promise.allSettled(promises).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+    return {
+      status: result.status,
+      value: result.reason, // not "reason" but "result.reason"
+    };
+  }));
 }
