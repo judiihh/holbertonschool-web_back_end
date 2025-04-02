9cff5c9be7fba312d11342072b8de53c224de287
Judith Espinal <j.espinalm12@gmail.com>
2025-04-02 08:51:32 -0400

    fix: update handleProfileSignup to properly handle Promise settlements

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,19 @@
 import signUpUser from './4-user-promise'; // import the signUpUser function
 import uploadPhoto from './5-photo-reject'; // import the uploadPhoto function
 
-export default function handleProfileSignup(firstName, lastName, fileName) { // return a promise
-  return Promise.allSettled([
+export default async function handleProfileSignup( // return a promise
+  firstName,
+  lastName,
+  fileName,
+) {
+  const promises = [
     signUpUser(firstName, lastName), // sign up the user
     uploadPhoto(fileName), // upload the photo
-  ]).then((results) => results.map((result) => ({
+  ];
+
+  const results = await Promise.allSettled(promises);
+  return results.map((result) => ({
     status: result.status, // return the status of the promise
     value: result.status === 'fulfilled' ? result.value : result.reason, // return the value of the promise
-  })));
+  }));
 }
