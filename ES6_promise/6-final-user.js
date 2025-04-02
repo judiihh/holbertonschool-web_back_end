5f3d418fce39c7b25c0c6fa023a3fb03eb3629a6
Judith Espinal <j.espinalm12@gmail.com>
2025-04-02 09:21:32 -0400

    fix: handleProfileSignup to correctly map 'reason' to 'value' for rejected promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,12 @@
-import signUpUser from './4-user-promise'; // import the signUpUser function
-import uploadPhoto from './5-photo-reject'; // import the uploadPhoto function
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  // create an array of promises
-
-  return Promise.allSettled(promises).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
-      status: result.status,
-      value: result.reason, // not "reason" but "result.reason"
-    };
-  }));
+export default function handleProfileSignup(firstName, lastName, fileName) { // return a promise
+  return Promise.allSettled([
+    signUpUser(firstName, lastName), // sign up the user
+    uploadPhoto(fileName), // upload the photo
+  ]).then((results) => results.map((item) => ({
+    status: item.status, // return the status of the promise
+    value: item.status === 'fulfilled' ? item.value : item.reason, // return the value of the promise
+  })));
 }
