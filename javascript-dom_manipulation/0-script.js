5c1ae7d8d2b3eebc75cf5755743ec196bf78eaf3
Judith Espinal <j.espinalm12@gmail.com>
2025-04-02 22:55:36 -0400

    delete: extra files

--- javascript-dom_manipulation/0-script.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env browser */
-// Select the header element using document.querySelector
-const header = document.querySelector('header');
-
-// Update the text color to red (#FF0000)
-header.style.color = '#FF0000';
