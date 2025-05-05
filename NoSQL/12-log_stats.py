93cc9a1c511970587d5836e06c25f06eb4cd317e
Judith Espinal <j.espinalm12@gmail.com>
2025-05-04 20:00:22 -0400

    fix: format

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,22 +8,21 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    # Count of logs
-    logs_count = logs_collection.count_documents({})
-    print(f"{logs_count} logs")
-    
-    # Count of methods
+    # Total logs count
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    # Methods count
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = logs_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-    # Count of status checks
-    status_check = logs_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
-    
-    # Close the MongoDB connection
-    client.close()
+    # Status check count
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(status_check_count))
