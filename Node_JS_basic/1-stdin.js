62f77dbe11e75e4f73ce5f851bfba12fd28bfa89
Judith Espinal <j.espinalm12@gmail.com>
2025-05-22 11:57:07 -0400

    correct task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
+// set the encoding to utf8
+process.stdin.setEncoding('utf8');
+// read the input
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk.toString().trim()}\r`);
+    process.stdout.write(`Your name is: ${chunk.toString().trim()}\n`);
   }
-});
+}); // end of readable event
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-}); 
+}); // end of end event
+
