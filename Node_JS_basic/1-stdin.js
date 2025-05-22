75b3b2261ce494055b42e94fe3c0511dc111d550
Judith Espinal <j.espinalm12@gmail.com>
2025-05-22 11:35:35 -0400

    fixing test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk.toString().trim()}\n`);
+    process.stdout.write(`Your name is: ${chunk.toString().trim()}\r`);
   }
 });
 
