3b8ee92f7640c8c11d254183bccb25732f672cc0
wilbert <wb995201@gmail.com>
2025-06-13 18:43:17 -0400

    fix: EOF handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().replace(/[\r\n]+$/, '');
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}\r\n`);
 });
 
 process.stdin.on('end', () => {
