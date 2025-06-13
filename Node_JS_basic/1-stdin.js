e9a97d868fd8d811eeb4b4d0f1cb14c9472c2819
wilbert <wb995201@gmail.com>
2025-06-13 18:35:46 -0400

    fixing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().replace(/\n$/, '');
+  const name = data.toString().replace(/[\r\n]+$/, '');
   process.stdout.write(`Your name is: ${name}\r`);
   process.exit();
 });
