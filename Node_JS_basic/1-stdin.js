f7c6797758d0aa73436c77c7dc5be344a8f5efbf
Judith <j.espinalm12@gmail.com>
2025-06-13 15:28:16 -0400

    add: task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// set the encoding to utf8
-process.stdin.setEncoding('utf8');
 
-// Use 'data' event to process incoming chunks
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  const lineEnding = process.stdout.isTTY ? '\r' : '\n';
-  process.stdout.write(`Your name is: ${name}${lineEnding}`);
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
-process.stdin.on('end', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
-}); // end of end event
+});
+ 
